efa742587e3caae13a2392016a5d0e8447925384
gonzalesisherkyle <gonzalesisherkyle1@gmail.com>
2023-10-06 14:17:31 +0800

    Added tailwind css to skills

--- src/components/Frontend/SkillComponent.vue
+++ src/components/Frontend/SkillComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-light-primary dark:bg-dark-primary py-5">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <dl class="grid grid-cols-3 gap-x-8 gap-y-16 lg:grid-cols-9">
+      <dl class="grid grid-cols-5 gap-x-8 gap-y-16 md:grid-cols-10">
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
           <img src="/storage/skills/php.svg" class="lg:h-20" />
         </div>
@@ -19,6 +19,9 @@
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
           <img src="/storage/skills/vue.svg" class="lg:h-20" />
+        </div>
+        <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
+          <img src="/storage/skills/tailwindcss.svg" class="lg:h-20" />
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
           <img src="/storage/skills/mysql.svg" class="lg:h-20" />
