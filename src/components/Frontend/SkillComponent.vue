2a848839dab933aacf5684d38034315cef57c978
gonzalesisherkyle <gonzalesisherkyle1@gmail.com>
2023-11-14 22:24:40 +0800

    Skills change

--- src/components/Frontend/SkillComponent.vue
+++ src/components/Frontend/SkillComponent.vue
@@ -3,16 +3,16 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <dl class="grid grid-cols-5 gap-x-8 gap-y-16 md:grid-cols-10">
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
+          <img src="/storage/skills/javascript.svg" class="lg:h-20" />
+        </div>
+        <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-evenly">
           <img src="/storage/skills/php.svg" class="lg:h-20" />
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
-          <img src="/storage/skills/javascript.svg" class="lg:h-20" />
+          <img src="/storage/skills/python.svg" class="lg:h-20" />
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
-          <img src="/storage/skills/html.svg" class="lg:h-20" />
-        </div>
-        <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
-          <img src="/storage/skills/css.svg" class="lg:h-20" />
+          <img src="/storage/skills/nodejs.svg" class="lg:h-20" />
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
           <img src="/storage/skills/laravel.svg" class="lg:h-20" />
@@ -27,10 +27,10 @@
           <img src="/storage/skills/mysql.svg" class="lg:h-20" />
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
-          <img src="/storage/skills/python.svg" class="lg:h-20" />
+          <img src="/storage/skills/html.svg" class="lg:h-20" />
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4 justify-around">
-          <img src="/storage/skills/github.svg" class="lg:h-20" />
+          <img src="/storage/skills/css.svg" class="lg:h-20" />
         </div>
       </dl>
     </div>
