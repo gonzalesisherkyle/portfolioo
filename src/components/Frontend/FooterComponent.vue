02d0026f4466d86f8a9c99cf0e7137bc29fd146a
Isher Kyle Gonzales <gonzalesisherkyle1@gmail.com>
2024-02-13 17:30:05 +0800

    Minor fixes

--- src/components/Frontend/FooterComponent.vue
+++ src/components/Frontend/FooterComponent.vue
@@ -5,7 +5,9 @@
     <div class="container mx-auto">
       <div class="flex flex-col md:flex-row space-y-6 lg:space-y-0 items-center justify-between">
         <div class="flex space-x-6 items-center justify-between">
-          <img class="h-14 w-auto" src="/storage/images/ig-logo.png" alt="my-logo" />
+          <a href="/">
+            <img class="h-14 w-auto" src="/storage/images/ig-logo.png" alt="my-logo" />
+          </a>
         </div>
         <p class="text-dark-primary dark:text-light-primary text-[15px]">
           &copy; 2023 Isher Kyle Gonzales
